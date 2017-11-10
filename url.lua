f55f05c171c240b1b6d72d6753481eac7f1db6ad
Masatoshi Teruya <mah0x211@gmail.com>
2017-11-10 14:36:04 +0900

    Use url.parse instead of uriparser

--- url.lua
+++ url.lua
@@ -1,17 +1,17 @@
 --[[
 
   Copyright (C) 2013 Masatoshi Teruya
- 
+
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
- 
+
   The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
- 
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -22,7 +22,6 @@
 
 --]]
 
-local uriparser = require('uriparser');
 local codec = require('url.codec');
 
 return {
@@ -32,7 +31,6 @@ return {
     decode2396 = codec.decode2396,
     encode3986 = codec.encode3986,
     decode3986 = codec.decode3986,
-    parse = uriparser.parse,
-    parseQuery = uriparser.parseQuery
+    parse = require('url.parse'),
 };
 
