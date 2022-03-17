c5d3b6a374369046fd6625593c3235396c6fa36c
Masatoshi Fukunaga <mah0x211@gmail.com>
2022-03-17 22:49:11 +0900

    Use luaL_Buffer instead of malloc

--- src/codec.c
+++ src/codec.c
@@ -1,4 +1,4 @@
-/*
+/**
  *  Copyright (C) 2014 Masatoshi Teruya
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a
@@ -199,52 +199,31 @@ static const char HEX2DEC[256] = {
 #define CODEC_UTF8ENC_LEN 12
 #define CODEC_UTF8DEC_LEN 4
 
-static unsigned char *encode(char *str, size_t slen, const unsigned char *tbl,
-                             size_t *len)
-{
-    unsigned char *dest = malloc(3 * slen + 1);
-
-    if (dest) {
-        unsigned char *src = (unsigned char *)str;
-        ssize_t bytes      = 0;
-        size_t i           = 0;
-
-        for (; i < slen; i++) {
-            if (tbl[*src]) {
-                dest[bytes++] = *src;
-            } else {
-                dest[bytes]     = '%';
-                // *src >> 4 = *src / 16
-                dest[bytes + 1] = DEC2HEX[*src >> 4];
-                // *src & 0xf = *src % 16
-                dest[bytes + 2] = DEC2HEX[*src & 0xf];
-                bytes += 3;
-            }
-            src++;
-        }
-
-        *len = bytes;
-    }
-
-    return dest;
-}
-
 static int encode_lua(lua_State *L, const unsigned char *tbl)
 {
-    size_t len      = 0;
-    const char *src = lauxh_checklstring(L, 1, &len);
-    char *dest      = (char *)encode((char *)src, len, tbl, &len);
+    size_t len            = 0;
+    unsigned char *src    = (unsigned char *)lauxh_checklstring(L, 1, &len);
+    unsigned char dest[3] = {'%', 0};
+    size_t i              = 0;
+    luaL_Buffer b         = {0};
 
-    if (dest) {
-        lua_pushlstring(L, dest, len);
-        return 1;
+    lua_settop(L, 1);
+    luaL_buffinit(L, &b);
+    for (; i < len; i++) {
+        if (tbl[*src]) {
+            luaL_addchar(&b, *src);
+        } else {
+            // *src >> 4 = *src / 16
+            dest[1] = DEC2HEX[*src >> 4];
+            // *src & 0xf = *src % 16
+            dest[2] = DEC2HEX[*src & 0xf];
+            luaL_addlstring(&b, (char *)dest, 3);
+        }
+        src++;
     }
 
-    // got error
-    lua_pushnil(L);
-    lua_pushinteger(L, errno);
-
-    return 2;
+    luaL_pushresult(&b);
+    return 1;
 }
 
 static int encodeuri_lua(lua_State *L)
@@ -265,13 +244,14 @@ static int encode3986_lua(lua_State *L)
     utf8 code-point: u+0000 ... u+10ffff = 0-1114111 = 21bit
               ascii: u+0000 ... u+007f   = 0-127     = 7bit
 */
-static int unicode_pt2utf8(unsigned char *dest, size_t *len, uint32_t cp)
+static int unicode_pt2utf8(luaL_Buffer *b, uint32_t cp)
 {
+    unsigned char dest[4] = {0};
+
     // range: u+0000 ... u+007f
     //   bit: 0xxx xxxx
     if (cp < 0x80) {
-        dest[0] = cp;
-        *len += 1;
+        luaL_addchar(b, cp);
     }
     // range: u+0080 ... u+07ff
     //   bit: [110y yyyx]:0xc0
@@ -279,7 +259,7 @@ static int unicode_pt2utf8(unsigned char *dest, size_t *len, uint32_t cp)
     else if (cp < 0x800) {
         dest[0] = 0xc0 | (cp >> 6);
         dest[1] = 0x80 | (cp & 0x3f);
-        *len += 2;
+        luaL_addlstring(b, (char *)dest, 2);
     }
     // invalid code-point
     // range: u+d800 ... u+dfff use for surrogate pairs
@@ -295,7 +275,7 @@ static int unicode_pt2utf8(unsigned char *dest, size_t *len, uint32_t cp)
         dest[0] = 0xe0 | (cp >> 12);
         dest[1] = 0x80 | ((cp >> 6) & 0x3f);
         dest[2] = 0x80 | (cp & 0x3f);
-        *len += 3;
+        luaL_addlstring(b, (char *)dest, 3);
     }
     // range: u+10000 ... u+1FFFFF
     //   bit: [1111 0yyy]:0xf0
@@ -310,7 +290,8 @@ static int unicode_pt2utf8(unsigned char *dest, size_t *len, uint32_t cp)
         dest[1] = 0x80 | ((cp >> 12) & 0x3f);
         dest[2] = 0x80 | ((cp >> 6) & 0x3f);
         dest[3] = 0x80 | (cp & 0x3f);
-        *len += 4;
+        luaL_addlstring(b, (char *)dest, 4);
+
     }
     /* UTF-8 now max 4 bytes
     // range: u+200000 ... u+3FFFFFF
@@ -358,113 +339,101 @@ static int unicode_pt2utf8(unsigned char *dest, size_t *len, uint32_t cp)
     unicode code-point: u+0000 ... u+10ffff = 21bit
                  ascii: u+0000 ... u+007f   = 0-127 = 7bit
 */
-static unsigned char *decode(char *str, size_t slen, const unsigned char *tbl,
-                             size_t *len)
+static int decode(lua_State *L, char *str, size_t slen,
+                  const unsigned char *tbl)
 {
-    unsigned char *dest = malloc(slen + 1);
+    unsigned char *src = (unsigned char *)str;
+    uint32_t hl, hi, lo;
+    size_t i      = 0;
+    luaL_Buffer b = {0};
 
-    if (dest) {
-        unsigned char *src = (unsigned char *)str;
-        uint32_t hl, hi, lo;
-        size_t bytes = 0;
-        size_t i     = 0;
+    luaL_buffinit(L, &b);
 
-        for (; i < slen; i++) {
-            if (*src != '%') {
-                dest[bytes++] = *src;
-                src++;
-            }
-            // percent-encoding(%hex) must have more than 2 byte strings after
-            // '%'.
-            else if (i + 2 > slen) {
-                goto INVALID_ENCODING;
-            }
-            /*
-                hex(8bit) to decimal
-                e.g.
-                    hex:'%41'
-                    '4' to hex:0x04[0000 0100]
-                    '1' to hex:0x01[0000 0001]
-                    0x04[0000 0100] << 4bit
-                    0x40[0100 0000] | 0x01[0000 0001]
-                    0x41[0100 0001]
-                    0x41 = 65 = 'A'
-
-                    hex:'%7a'
-                    '7' to hex:0x07[0000 0111]
-                    'a' to hex:0x0a[0000 1010]
-                    0x07[0000 0111] << 4bit
-                    0x70[0111 0000] | 0x0a[0000 1010]
-                    0x7a[0111 1010]
-                    0x7a:122 = 'z'
-
-                    hex:'%7a4'
-                    '7' to hex:0x07[0000 0111]
-                    'a' to hex:0x0a[0000 1010]
-                    '4' to hex:0x04[0000 0100]
-                    0x007[0000 0000 0111] << 8bit
-                    0x00a[0000 0000 1010] << 4bit
-                    0x70[0111 0000 0000] | [0000 1010 0000] | 0x04[0000 0100]
-                    0x7a4[0111 1010 0100] = 1956
-            */
-            // %[hex]*2
-            else if (HEX2DEC[src[1]] > -1 && HEX2DEC[src[2]] > -1) {
-                /*
-                    hi = HEX2DEC( src[1] ) << 4;
-                    lo = HEX2DEC( src[2] );
-                    hl = hi | lo;
-                */
-                hl = (HEX2DEC[src[1]] << 4) | HEX2DEC[src[2]];
-                if (!tbl[hl]) {
-                    dest[bytes++] = hl;
-                } else {
-                    dest[bytes]     = src[0];
-                    dest[bytes + 1] = src[1];
-                    dest[bytes + 2] = src[2];
-                    bytes += 3;
-                }
-                i += 2;
-                src += 3;
-            }
-            // %u[hex]*4
-            else if (src[1] == 'u' && HEX2DEC[src[2]] > -1 &&
-                     HEX2DEC[src[3]] > -1 && HEX2DEC[src[4]] > -1 &&
-                     HEX2DEC[src[5]] > -1) {
-                hi = HEX2DEC[src[2]] << 4 | HEX2DEC[src[3]];
-                lo = HEX2DEC[src[4]] << 4 | HEX2DEC[src[5]];
-                hl = (hi << 8) | lo;
-                if (unicode_pt2utf8(dest + bytes, &bytes, hl) == -1) {
-                    goto INVALID_ENCODING;
-                }
-                i += 4;
-                src += 6;
-            } else {
-                goto INVALID_ENCODING;
-            }
+    for (; i < slen; i++) {
+        if (*src != '%') {
+            luaL_addchar(&b, *src);
+            src++;
         }
+        // percent-encoding(%hex) must have more than 2 byte strings after '%'.
+        else if (i + 2 > slen) {
+            errno = EINVAL;
+            return -1;
+        }
+        /*
+            hex(8bit) to decimal
+            e.g.
+                hex:'%41'
+                '4' to hex:0x04[0000 0100]
+                '1' to hex:0x01[0000 0001]
+                0x04[0000 0100] << 4bit
+                0x40[0100 0000] | 0x01[0000 0001]
+                0x41[0100 0001]
+                0x41 = 65 = 'A'
 
-        *len = bytes;
+                hex:'%7a'
+                '7' to hex:0x07[0000 0111]
+                'a' to hex:0x0a[0000 1010]
+                0x07[0000 0111] << 4bit
+                0x70[0111 0000] | 0x0a[0000 1010]
+                0x7a[0111 1010]
+                0x7a:122 = 'z'
 
-        return dest;
-
-INVALID_ENCODING:
-        errno = EINVAL;
-        free(dest);
-        dest = NULL;
+                hex:'%7a4'
+                '7' to hex:0x07[0000 0111]
+                'a' to hex:0x0a[0000 1010]
+                '4' to hex:0x04[0000 0100]
+                0x007[0000 0000 0111] << 8bit
+                0x00a[0000 0000 1010] << 4bit
+                0x70[0111 0000 0000] | [0000 1010 0000] | 0x04[0000 0100]
+                0x7a4[0111 1010 0100] = 1956
+        */
+        // %[hex]*2
+        else if (HEX2DEC[src[1]] > -1 && HEX2DEC[src[2]] > -1) {
+            /*
+                hi = HEX2DEC( src[1] ) << 4;
+                lo = HEX2DEC( src[2] );
+                hl = hi | lo;
+            */
+            hl = (HEX2DEC[src[1]] << 4) | HEX2DEC[src[2]];
+            if (!tbl[hl]) {
+                luaL_addchar(&b, hl);
+            } else {
+                luaL_addlstring(&b, (char *)src, 3);
+            }
+            i += 2;
+            src += 3;
+        }
+        // %u[hex]*4
+        else if (src[1] == 'u' && HEX2DEC[src[2]] > -1 &&
+                 HEX2DEC[src[3]] > -1 && HEX2DEC[src[4]] > -1 &&
+                 HEX2DEC[src[5]] > -1) {
+            hi = HEX2DEC[src[2]] << 4 | HEX2DEC[src[3]];
+            lo = HEX2DEC[src[4]] << 4 | HEX2DEC[src[5]];
+            hl = (hi << 8) | lo;
+            if (unicode_pt2utf8(&b, hl) == -1) {
+                errno = EINVAL;
+                return -1;
+            }
+            i += 4;
+            src += 6;
+        } else {
+            errno = EINVAL;
+            return -1;
+        }
     }
 
-    return dest;
+    luaL_pushresult(&b);
+
+    return 0;
 }
 
 static int decode_lua(lua_State *L, const unsigned char *tbl)
 {
     size_t len      = 0;
     const char *src = lauxh_checklstring(L, 1, &len);
-    char *dest      = (char *)decode((char *)src, len, tbl, &len);
 
-    if (dest) {
-        lua_pushlstring(L, dest, len);
-        free(dest);
+    lua_settop(L, 1);
+    if (decode(L, (char *)src, len, tbl) == 0) {
         return 1;
     }
 
